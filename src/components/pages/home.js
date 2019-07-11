fe7fab189d5be3d43c293fdf6c9b3c4209b5b73c
alexjones <ajonzy@gmail.com>
2019-07-11 16:26:35 -0600

    Added course selection section

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -14,6 +14,56 @@ export default function home() {
                         <h4>Bottega’s best in class curriculum delivers programs that are backed by The American College of Education (<a href="http://www2.acenet.edu/credit/?fuseaction=browse.getOrganizationDetail&amp;FICE=1009331">ACE</a>). This accreditation recommends college credit be awarded for Bottega courses at many universities.</h4>
                     </div>          
                 </div>
+
+                <div className="course-display-section-wrapper">
+                    <div className="course-display-header">
+                        <span>Choose Your Course</span>
+                        <h5 class="p1">We offer the most up to date computer science tracks in the world, because we work with top employers and update them every 90 days.</h5>
+                        <h5 class="p2">Our courses are designed to teach you through deep hands on learning to get you into the job market in the career of your dreams, with the help you need to understand and stay motivated every step of the way.</h5>
+                    </div>
+
+                    <div className="course-display">
+                        <div className="course-block">
+                            <img src="https://bottega.tech/wp-content/uploads/2018/03/Python-JS.png" alt=""/>
+                            <h6 className="course-title">Full Stack Python + React</h6>
+                            <h6 className="course-subtitle">Beginner</h6>
+                            <h5 className="course-description">Explosive demand, Engineering track</h5>
+                            <div className="course-button">
+                                <a href="#">Learn More</a>
+                            </div>
+                        </div>
+
+                        <div className="course-block">
+                            <img src="https://bottega.tech/wp-content/uploads/2018/03/Front-End.png" alt=""/>
+                            <h6 className="course-title">Front End Development</h6>
+                            <h6 className="course-subtitle">Beginner</h6>
+                            <h5 className="course-description">Build visually, highly employable skills</h5>
+                            <div className="course-button">
+                                <a href="#">Learn More</a>
+                            </div>
+                        </div>
+
+                        <div className="course-block">
+                            <img src="https://bottega.tech/wp-content/uploads/2018/03/Ruby-Rails.png" alt=""/>
+                            <h6 className="course-title">Full Stack Ruby</h6>
+                            <h6 className="course-subtitle">Advanced</h6>
+                            <h5 className="course-description">High paying salary, specialization</h5>
+                            <div className="course-button">
+                                <a href="#">Learn More</a>
+                            </div>
+                        </div>
+
+                        <div className="course-block">
+                            <img src="https://bottega.tech/wp-content/uploads/2018/03/Machine-Learning.png" alt=""/>
+                            <h6 className="course-title">Data Science</h6>
+                            <h6 className="course-subtitle">Advanced</h6>
+                            <h5 className="course-description">Bleeding edge problem solving, logic</h5>
+                            <div className="course-button">
+                                <a href="#">Learn More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
