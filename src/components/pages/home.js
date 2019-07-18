86932118bebb2794e23d0d366b1b3d6e234be219
alexjones <ajonzy@gmail.com>
2019-07-18 14:27:55 -0600

    Added legal section

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -336,6 +336,16 @@ export default function home() {
                         </div>
                     </div>
                 </div>
+            
+                <div className="legal-section-wrapper">
+                    <div className="legal-section-content">
+                        <ol>
+                            <li>
+                                <h6>Bottega is Accredited by the Northwest Accreditation Commission |&nbsp;<a href="http://www.advanc-ed.org/oasis2/u/par/accreditation/summary?institutionId=50382"><span class="s1">AdvancED</span></a>&nbsp;as a Post-Secondary non-degree granting school. AdvancED is a private accreditation body and <strong><em>not recognized by the U.S. Department of Education</em></strong>. Additionally, Bottega received CREDIT recommendation recognition through the&nbsp;<a href="http://www.acenet.edu/news-room/Pages/National-Guide-to-College-Credit-for-Workforce-Training.aspx"><span class="s1">American Council on Education</span></a>&nbsp;to provide CS credit and transcript service through ACE. Explore the benefits of having current curriculum and receive both your industry credential and a path to college credit.</h6>
+                            </li>
+                        </ol>
+                    </div>
+                </div>
             </div>
         </div>
     )
