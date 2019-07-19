4d0194df1b3f2986711c151c5782e940c6d7e25f
alexjones <ajonzy@gmail.com>
2019-07-19 13:36:39 -0600

    Cleaned up various warnings

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -129,7 +129,7 @@ export default function home() {
                             </p>
                             <a href="https://apply.bottega.tech/" className="apply-wrapper">
                                 Apply&nbsp;now
-                                <p><div>></div></p>
+                                <p><span className="apply-arrow">></span></p>
                             </a>
                         </div>
 
@@ -145,7 +145,7 @@ export default function home() {
                             </p>
                             <a href="https://apply.bottega.tech/" className="apply-wrapper">
                                 Apply&nbsp;now
-                                <p><div>></div></p>
+                                <p><span className="apply-arrow">></span></p>
                             </a>
                         </div>
 
@@ -161,7 +161,7 @@ export default function home() {
                             </p>
                             <a href="https://apply.bottega.tech/" className="apply-wrapper">
                                 Apply&nbsp;now
-                                <p><div>></div></p>
+                                <p><span className="apply-arrow">></span></p>
                             </a>
                         </div>
                     </div>
@@ -413,7 +413,7 @@ export default function home() {
                     <div className="legal-section-content">
                         <ol>
                             <li>
-                                <h6>Bottega is Accredited by the Northwest Accreditation Commission |&nbsp;<a href="http://www.advanc-ed.org/oasis2/u/par/accreditation/summary?institutionId=50382"><span class="s1">AdvancED</span></a>&nbsp;as a Post-Secondary non-degree granting school. AdvancED is a private accreditation body and <strong><em>not recognized by the U.S. Department of Education</em></strong>. Additionally, Bottega received CREDIT recommendation recognition through the&nbsp;<a href="http://www.acenet.edu/news-room/Pages/National-Guide-to-College-Credit-for-Workforce-Training.aspx"><span class="s1">American Council on Education</span></a>&nbsp;to provide CS credit and transcript service through ACE. Explore the benefits of having current curriculum and receive both your industry credential and a path to college credit.</h6>
+                                <h6>Bottega is Accredited by the Northwest Accreditation Commission |&nbsp;<a href="http://www.advanc-ed.org/oasis2/u/par/accreditation/summary?institutionId=50382"><span className="s1">AdvancED</span></a>&nbsp;as a Post-Secondary non-degree granting school. AdvancED is a private accreditation body and <strong><em>not recognized by the U.S. Department of Education</em></strong>. Additionally, Bottega received CREDIT recommendation recognition through the&nbsp;<a href="http://www.acenet.edu/news-room/Pages/National-Guide-to-College-Credit-for-Workforce-Training.aspx"><span className="s1">American Council on Education</span></a>&nbsp;to provide CS credit and transcript service through ACE. Explore the benefits of having current curriculum and receive both your industry credential and a path to college credit.</h6>
                             </li>
                         </ol>
                     </div>
--- src/components/resources/headerMain.js
+++ src/components/resources/headerMain.js
@@ -24,7 +24,6 @@ export default function headerMain() {
             }
 
             didMountRef.current = true
-            console.log(sticky);
         }
     })
 
