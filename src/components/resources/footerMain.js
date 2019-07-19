d5b920312fbaebb2f86a1c6277aac7ec7218bc97
alexjones <ajonzy@gmail.com>
2019-07-19 10:28:17 -0600

    Added footerMain component skeleton

---
+++ src/components/resources/footerMain.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+import Logo from "../../../static/assets/images/bottega-logo-black.png"
+
+export default function footerMain() {
+    return (
+        <div className="footer-main">
+            
+        </div>
+    )
+}
