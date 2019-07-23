82a75eda93ec1bf163519e617a56e361060d5108
alexjones <ajonzy@gmail.com>
2019-07-23 15:19:55 -0600

    Finished the schedule a call page

--- src/components/pages/scheduleCall.js
+++ src/components/pages/scheduleCall.js
@@ -3,6 +3,11 @@ import React from 'react'
 import Header from "../resources/headerMain"
 import Footer from "../resources/footerMain"
 
+import USFlag from "../../../static/assets/images/scheduleCall/us-flag.png"
+import MexicoFlag from "../../../static/assets/images/scheduleCall/mexico-flag.jpg"
+import JamaicaFlag from "../../../static/assets/images/scheduleCall/jamaica-flag.jpg"
+
+
 export default function scheduleCall() {
     return (
         <div className="schedule-call">
@@ -13,9 +18,9 @@ export default function scheduleCall() {
                     <h5>Select The Country You Reside In.</h5>
                     <h5>Seleccione el país en el que reside.</h5>
                     <div className="country-links">
-                        <a href=""></a>
-                        <a href=""></a>
-                        <a href=""></a>
+                        <a href="https://calendly.com/bottega-advisors/phonecall"><img src={USFlag} alt=""/></a>
+                        <a href="https://calendly.com/bottega-advisors/llamada"><img src={MexicoFlag} alt=""/></a>
+                        <a href="https://calendly.com/bottega-advisors/phonecall"><img src={JamaicaFlag} alt=""/></a>
                     </div>
                 </div>
             </div>
