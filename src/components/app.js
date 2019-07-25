391fbb70a06a29e990d8bd23be8365e639e99928
alexjones <ajonzy@gmail.com>
2019-07-25 10:24:41 -0600

    Added skeleton for python-react-fulltime page

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,13 @@ import {useRoutes} from 'hookrouter';
 
 import Home from "./pages/home"
 import ScheduleCall from "./pages/scheduleCall"
+import PythonReactFulltime from "./pages/pythonReactFulltime"
 
 export default function app() {
   const routes = {
     "/": () => <Home />,
-    "/schedule-call": () => <ScheduleCall />
+    "/schedule-call": () => <ScheduleCall />,
+    "/python-react-full-time": () => <PythonReactFulltime />
   };
   
   const routeResult = useRoutes(routes)
--- src/components/pages/pythonReactFulltime.js
+++ src/components/pages/pythonReactFulltime.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import Header from "../resources/headerMain"
+import Footer from "../resources/footerMain"
+
 export default function pythonReactFulltime() {
     return (
-        <div>
-            
+        <div className="python-react-fulltime">
+            <Header />
+            <div className="python-react-fulltime-content-wrapper">
+                
+            </div>
+            <Footer />
         </div>
     )
 }
