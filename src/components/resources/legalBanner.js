9b00fb1c7ef84f31159dabcb40a210aef66305a2
alexjones <ajonzy@gmail.com>
2019-08-01 09:31:43 -0600

    Broke out the legal banner section from the homepage

---
+++ src/components/resources/legalBanner.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function legalBanner() {
+    return (
+        <div className="legal-banner">
+            <div className="legal-banner-content">
+                <ol>
+                    <li>
+                        <h6>Bottega is Accredited by the Northwest Accreditation Commission |&nbsp;<a href="http://www.advanc-ed.org/oasis2/u/par/accreditation/summary?institutionId=50382"><span className="s1">AdvancED</span></a>&nbsp;as a Post-Secondary non-degree granting school. AdvancED is a private accreditation body and <strong><em>not recognized by the U.S. Department of Education</em></strong>. Additionally, Bottega received CREDIT recommendation recognition through the&nbsp;<a href="http://www.acenet.edu/news-room/Pages/National-Guide-to-College-Credit-for-Workforce-Training.aspx"><span className="s1">American Council on Education</span></a>&nbsp;to provide CS credit and transcript service through ACE. Explore the benefits of having current curriculum and receive both your industry credential and a path to college credit.</h6>
+                    </li>
+                </ol>
+            </div>
+        </div>
+    )
+}
