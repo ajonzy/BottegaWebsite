f7537e456ccdb9e19b7f5f700cfd68eca83f8f3e
alexjones <ajonzy@gmail.com>
2019-08-01 09:45:30 -0600

    Added the fulltime remote page

--- src/components/app.js
+++ src/components/app.js
@@ -4,12 +4,14 @@ import {useRoutes} from 'hookrouter';
 import Home from "./pages/home"
 import ScheduleCall from "./pages/scheduleCall"
 import PythonReactFulltime from "./pages/pythonReactFulltime"
+import PythonReactFulltimeRemote from "./pages/pythonReactFulltimeRemote"
 
 export default function app() {
   const routes = {
     "/": () => <Home />,
     "/schedule-call": () => <ScheduleCall />,
-    "/python-react-full-time": () => <PythonReactFulltime />
+    "/python-react-full-time": () => <PythonReactFulltime />,
+    "/python-react-full-time-remote": () => <PythonReactFulltimeRemote />
   };
   
   const routeResult = useRoutes(routes)
--- src/components/pages/pythonReactFulltimeRemote.js
+++ src/components/pages/pythonReactFulltimeRemote.js
@@ -1,9 +1,51 @@
 import React from 'react'
 
+import Header from "../resources/headerMain"
+import Footer from "../resources/footerMain"
+import HeaderVideo from "../resources/headerVideo"
+import LegalBanner from "../resources/legalBanner"
+import SyllabusDownload from "../resources/courses/syllabusDownload"
+import CourseDescription from "../resources/courses/courseDescription"
+import DatesAndOptions from "../resources/courses/datesAndOptions"
+import ApplicationProcess from "../resources/courses/applicationProcess"
+import TuitionCalculator from "../resources/courses/tuitionCalculator"
+import EmployableSkillset from "../resources/courses/employableSkillset"
+import LearningExperience from "../resources/courses/learningExperience"
+import DontWaitBanner from "../resources/courses/dontWaitBanner"
+import LanguageSummaries from "../resources/courses/languageSummaries"
+import CourseCurriculum from "../resources/courses/courseCurriculum"
+import DailySchedule from "../resources/courses/dailySchedule"
+import CourseFAQ from "../resources/courses/courseFAQ"
+
+import FulltimeVideo from "../../../static/assets/images/pythonReactCourses/fulltimeVideo.mp4"
+import Syllabus from "../../../static/assets/downloads/FulltimeSyllabus.pdf"
+
 export default function pythonReactFulltimeRemote() {
     return (
-        <div>
-            
+        <div className="python-react-fulltime-remote">
+            <Header />
+            <div className="python-react-fulltime-remote-content-wrapper">
+                <HeaderVideo 
+                    video={FulltimeVideo}
+                    superHeader={<h5>Full Stack Development (Full-Time)</h5>}
+                    header={<h1>Remote Python 3<br/>and React Course</h1>}
+                    subHeader={<h4>Attend our Full Time course remotely via video stream. <br/>Get trained on three of the most in-demand coding <br/>technologies in computer science; Python, React & JavaScript.</h4>}
+                />
+                <SyllabusDownload syllabus={Syllabus} />
+                <CourseDescription course="remote" />
+                <DatesAndOptions />
+                <ApplicationProcess />
+                <TuitionCalculator />
+                <EmployableSkillset />
+                <LearningExperience />
+                <DontWaitBanner />
+                <LanguageSummaries />
+                <CourseCurriculum course="main" />
+                <DailySchedule />
+                <CourseFAQ />
+                <LegalBanner />
+            </div>
+            <Footer />
         </div>
     )
 }
