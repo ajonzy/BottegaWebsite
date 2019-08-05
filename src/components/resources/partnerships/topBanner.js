7423f76f15a527094a53ce99ececbedd4a6a9e72
alexjones <ajonzy@gmail.com>
2019-08-05 13:12:07 -0600

    Added animation to top banner button

--- src/components/resources/partnerships/topBanner.js
+++ src/components/resources/partnerships/topBanner.js
@@ -1,14 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import BannerImage from "../../../../static/assets/images/partnerships/top-banner.jpg"
 
 export default function topBanner(props) {
+    useEffect(() => {
+        const button = document.getElementById("button")
+
+        const buttonLocation = button.offsetTop
+
+        window.addEventListener("scroll", () => {
+            if (window.pageYOffset >= (buttonLocation - window.innerHeight)) {
+                button.classList.add("triggered")
+            }
+        })
+    })
+
     const bannerContent = {
         university: (
             <div className="top-banner-content-wrapper">
                 <div className="top-banner-content">
                     <h3>University Partnerships</h3>
-                    <a href="#UniversityPartners">Partner with Bottega</a>
+                    <a href="#UniversityPartners" id="button">Partner with Bottega</a>
                 </div>
             </div>
         )
